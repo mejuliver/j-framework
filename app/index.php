0a0f3c23b08ae8b8f15ce8bc73f80dc08a1b5dcc
Juliver Galleto <juliver.galleto@gmail.com>
2018-03-23 14:16:50 +0800

    adjustmnts

--- app/index.php
+++ app/index.php
@@ -1,9 +1,44 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>test</title>
+	<title>Welcome</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<style>
+		@import url('https://fonts.googleapis.com/css?family=Raleway:300');
+		body{
+			color:#000;
+			font: normal 13px 'Raleway',sans-serif;
+			background:#0099ff;
+			-webkit-font-smoothing: antialiased;
+			-moz-osx-font-smoothing: grayscale;
+		}
+		#main-wrapper{
+			display:flex;
+			align-items: center;
+			justify-content: center;
+			color:#000;
+			height:100vh;
+		}
+		h1{
+			font-size:40px;
+			color:#FFF;
+			width:60%;
+			text-align:center;
+			text-shadow:1px 1px 1px rgba(0,0,0,0.2);
+			font-family: 'Raleway', sans-serif;
+			letter-spacing:2px;
+		}
+		@media(max-width:767px){
+			h1{
+				width:100%;
+				padding:40px;
+			}
+		}
+	</style>
 </head>
 <body>
-	<h1>test</h1>
+	<div id="main-wrapper">
+		<h1>Shoot to the moon, even if you miss you'll  land among the stars...</h1>
+	</div>
 </body>
 </html>
